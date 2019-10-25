f07315e41745f1e5d9035e32420c529dfedcc2b7
janka-m <31926681+janka-m@users.noreply.github.com>
2019-10-25 09:52:58 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package mittelwertBeispiel;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rechenprogramm rechenprogramm = new Rechenprogramm(Arrays.asList(100.0,200.0,300.0));
+		rechenprogramm.tuWas();
+		
+	}
+
+}
